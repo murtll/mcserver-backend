3779ecbff4941b251e0605b690a52c02f6eea429
murtll <jkoz@inbox.ru>
2022-02-02 23:57:46 +0300

    Add db and admin

---
+++ backend/db.js
@@ -0,0 +1,72 @@
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
+import crypto from 'crypto'
+
+var db = null
+
+export const openDb = async () => {
+    sqlite3.verbose()
+
+    db = await open({
+        filename: 'data/database.db',
+        driver: sqlite3.Database
+    })
+}
+
+export const selectItems = async (category) => {
+    const catId = (await db.get(`select id from categories where link='/${category}'`)).id
+    return await db.all('SELECT * FROM items where category_id = ?', [catId])
+}
+
+export const selectCategories = () => {
+    return db.all('SELECT * FROM categories')
+}
+
+export const addItem = async (item) => {
+    const catId = (await db.get(`select id from categories where link='/${item.category}'`)).id
+    await db.run('INSERT INTO items (picture, name, description, category_id, price) values (?, ?, ?, ?, ?)', [item.picture, item.name, item.description, catId, item.price])
+}
+
+export const deleteItem = (item) => {
+    return db.run('DELETE from items where id = ?', [item.id])
+}
+
+export const updateItem = (item) => {
+    let updates = Object.keys(item)
+                    .filter((prop) => prop != 'id')
+                    .map((prop) => {
+                        if (typeof item[prop] === 'number')
+                            return `${prop} = ${item[prop]}`
+                        return `${prop} = '${item[prop]}'`
+                    }).join(', ')
+
+    console.log(updates);
+
+    return db.run(`UPDATE items SET ${updates} where id = ?`, item.id)
+}
+
+export const addCategory = (category) => {
+    return db.run('INSERT INTO categories (name, link) values (?, ?)', [category.name, category.link])
+}
+
+export const deleteCategory = (category) => {
+    return db.run('DELETE from categories where id = ?', [category.id])
+}
+
+export const updateCategory = (category) => {
+    let updates = Object.keys(category)
+                    .filter((prop) => prop != 'id')
+                    .map((prop) => {
+                        if (typeof category[prop] === 'number')
+                            return `${prop} = ${category[prop]}`
+                        return `${prop} = '${category[prop]}'`
+                    }).join(', ')
+
+    console.log(updates);
+
+    return db.run(`UPDATE categories SET ${updates} where id = ?`, category.id)
+}
+
+export const checkAuthKey = async (key) => {
+    return crypto.createHash('sha256').update(key).digest('hex') === (await db.get('SELECT key FROM auth where id = 1')).key
+}
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import fs from 'fs'
 import cors from 'cors'
+import * as db from './db.js'
 
 const PORT=3001
 
 const app = express()
+
+db.openDb()
 
 app.use(express.json())
 app.use(cors())
@@ -14,10 +17,127 @@ app.get('/', (req, res) => {
     res.json({ status: 'OK' })
 })
 
-app.get('/:file', async (req, res) => {
+app.get('/categories', async (req, res) => {
     try {
-        const ans = JSON.parse(await fs.promises.readFile(`./data/${req.params.file}.json`))
+        const ans = await db.selectCategories()
         res.json(ans)
+    } catch (error) {
+        res.status(400).json({ error: error })
+    }
+})
+
+app.get('/:category', async (req, res) => {
+    try {
+        const ans = await db.selectItems(req.params.category)
+        res.json(ans)
+    } catch (error) {
+        res.status(400).json({ error: error })
+    }
+})
+
+app.post('/admin/item', async (req, res) => {
+    try {
+        if (!(await db.checkAuthKey(req.headers.authorization))) {
+            res.status(400).json({
+                error: 'Invalid key'
+            })
+            return
+        }
+
+        await db.addItem(req.body)
+
+        res.json({ added: req.body })
+
+    } catch (error) {
+        res.status(400).json({ error: error })
+    }
+})
+
+app.put('/admin/item', async (req, res) => {
+    try {
+        if (!(await db.checkAuthKey(req.headers.authorization))) {
+            res.status(400).json({
+                error: 'Invalid key'
+            })
+            return
+        }
+
+        await db.updateItem(req.body)
+
+        res.json({ updated: req.body })
+
+    } catch (error) {
+        res.status(400).json({ error: error })
+    }
+})
+
+app.delete('/admin/item', async (req, res) => {
+    try {
+        if (!(await db.checkAuthKey(req.headers.authorization))) {
+            res.status(400).json({
+                error: 'Invalid key'
+            })
+            return
+        }
+
+        await db.deleteItem(req.body)
+
+        res.json({ deleted: req.body })
+
+    } catch (error) {
+        res.status(400).json({ error: error })
+    }
+})
+
+app.post('/admin/category', async (req, res) => {
+    try {
+        if (!(await db.checkAuthKey(req.headers.authorization))) {
+            res.status(400).json({
+                error: 'Invalid key'
+            })
+            return
+        }
+
+        await db.addCategory(req.body)
+
+        res.json({ added: req.body })
+
+    } catch (error) {
+        res.status(400).json({ error: error })
+    }
+})
+
+app.put('/admin/category', async (req, res) => {
+    try {
+        if (!(await db.checkAuthKey(req.headers.authorization))) {
+            res.status(400).json({
+                error: 'Invalid key'
+            })
+            return
+        }
+
+        await db.updateCategory(req.body)
+
+        res.json({ updated: req.body })
+
+    } catch (error) {
+        res.status(400).json({ error: error })
+    }
+})
+
+app.delete('/admin/category', async (req, res) => {
+    try {
+        if (!(await db.checkAuthKey(req.headers.authorization))) {
+            res.status(400).json({
+                error: 'Invalid key'
+            })
+            return
+        }
+
+        await db.deleteCategory(req.body)
+
+        res.json({ deleted: req.body })
+
     } catch (error) {
         res.status(400).json({ error: error })
     }
