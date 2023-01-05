c25eb29fcdabc1e664058973b4175ae7bc761728
murtll <57870903+murtll@users.noreply.github.com>
2023-01-05 18:35:58 +0300

    rollback cookie settings

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(session({
     cookie: {
         sameSite: 'strict',
         maxAge: 3600000, 
-        path: '/api/admin', 
+        path: '/admin', 
     },
     saveUninitialized: false
 }))
