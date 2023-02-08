7b44ab8a37505ccb4e246e52915bd7dc01a1fb3d
murtll <jkoz@inbox.ru>
2023-02-08 10:09:27 +0300

    fix categories seeds.

--- seeds/01_categories.js
+++ seeds/01_categories.js
@@ -5,7 +5,7 @@
 export async function seed(knex) {
   await knex('categories').insert([
     {id: 1, name: 'Кейсы', link: '/cases'},
-    {id: 2, name: 'Привилегии', link: '/cases'},
+    {id: 2, name: 'Привилегии', link: '/privileges'},
     {id: 3, name: 'Гемы', link: '/money'},
     {id: 4, name: 'Особое', link: '/special'},
   ]);
